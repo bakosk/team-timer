01b4806f2b37c9b893cd444a3c5d77ec1dfed11e
matej.martiniak <martiniak.matej@gmail.com>
2025-04-04 21:55:41 +0200

    Switch to Svelte static adapter for deployment
    Replaced `@sveltejs/adapter-auto` with `@sveltejs/adapter-static` to enable static site generation. Updated configuration in `svelte.config.js` to define static adapter options, including build paths and fallback handling.

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,20 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+
+	kit: {
+		adapter: adapter({
+			// default options are shown
+			pages: 'build',
+			assets: 'build',
+			fallback: 'index.html',
+			precompress: false,
+			strict: true
+		})
+	}
 };
 
 export default config;
